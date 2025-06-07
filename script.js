3f754c10911b382f43b90587807c2abd10e18923
Pratham-Bhayana <prathambhayana868@gmail.com>
2025-06-07 11:47:24 +0530

    holly shit

---
+++ script.js
@@ -0,0 +1,56 @@
+gsap.from(".head-logo",{
+        scale:1.5,
+    delay:0.5,
+    duration:2,
+    y:-60,
+    opacity:0,
+});
+
+gsap.from("body",{
+    opacity:0,
+    duration:1.5,
+})
+
+gsap.from("header",{
+
+    duration:1.5,
+    y:-60,
+    opacity:0,
+    delay:0.5
+});
+
+gsap.from(".heading",{
+    duration:1.5,
+    x:-60,
+    opacity:0,
+    delay:0.5
+});
+
+gsap.from(".para",{
+    duration:1.5,
+    x:60,
+    opacity:0,
+    delay:0.5
+});
+
+gsap.from(".btns",{
+    
+
+    duration:1.5,
+    x:-60,
+    opacity:0,
+    delay:0.5
+});
+
+gsap.from(".imgdiv",{
+    delay:5,
+
+
+    duration:1.5,
+    y:60,
+
+    opacity:0,
+    delay:0.5
+});
+
+
